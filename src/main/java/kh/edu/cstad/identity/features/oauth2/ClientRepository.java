49e5ff60eae041b1f493fdde90e0968c7add5ecd
Polin <chaingpolin@gmail.com>
2025-12-11 12:01:51 +0700

    Implement Core Services With JPA

---
+++ src/main/java/kh/edu/cstad/identity/features/oauth2/ClientRepository.java
@@ -0,0 +1,12 @@
+package kh.edu.cstad.identity.features.oauth2;
+
+import kh.edu.cstad.identity.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
